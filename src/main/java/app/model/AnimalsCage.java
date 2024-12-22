043a32b848ad1b415d216f7d1ecbf559daa77fa4
volkovgit <guthub.volkov@gmail.com>
2024-12-22 17:39:28 +0300

    Исправил нназвание бина Dog->dog

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier("Dog")
+    @Qualifier("dog")
     private Animal animal;
 
     @Autowired
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -2,7 +2,7 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component("Dog")
+@Component("dog")
 public class Dog extends Animal{
     @Override
     public String toString(){
